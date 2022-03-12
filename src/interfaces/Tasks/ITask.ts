ae44ac39d3ce3042e6db30efd37589465a9f3390
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-03-12 17:53:51 -0300

    adiciona interfaces para tarefas

---
+++ src/interfaces/Tasks/ITask.ts
@@ -0,0 +1,7 @@
+interface ITask {
+  _id: string;
+  contents: string;
+  completed: boolean;
+}
+
+export default ITask;
---
+++ src/interfaces/Tasks/ITaskList.ts
@@ -0,0 +1,8 @@
+import ITask from './ITask';
+
+interface ITaskList {
+  userId: string;
+  tasks: ITask[];
+}
+
+export default ITaskList;
