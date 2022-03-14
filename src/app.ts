16cf2e4c414e217f847854cff708755e79457da4
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-03-14 19:10:51 -0300

    configura body-parse e cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const port = process.env.PORT as string;
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
-app.listen(port, () => {
-  console.log(`Application listening on port ${port}`);
-});
+app.use(bodyParser.json())
+
+app.use(cors());
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import app from './app';
 import * as routes from './routes';
+import dotenv from 'dotenv';
 
-app.use(routes.users);
+dotenv.config();
 
-app.use(routes.tasks);
+const port = process.env.PORT as string;
+
+app.listen(port, () => {
+  console.log(`Application listening on port ${port}`);
+});
+
+app.use('/users', routes.users);
+
+app.use('/tasks', routes.tasks);
