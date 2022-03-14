5faedfe413baf20b236db1a57c978bba748b7cf6
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-03-14 20:33:40 -0300

    cria rotas para criação e consultas de usuários

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,7 +1,21 @@
 import { Router } from 'express';
+import userController from '../controllers/Users'
 
 const route = Router();
 
-route.post('/');
+route.post(
+  '/',
+  userController.prototype.create,
+);
+
+route.get(
+  '/',
+  userController.prototype.getAll,
+);
+
+route.get(
+  '/:id',
+  userController.prototype.getById,
+);
 
 export default route;
